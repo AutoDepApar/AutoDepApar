6f7e18627da858fe6456817c5fe9c6506d9282f7
Maheyan <68837961+Maheyan@users.noreply.github.com>
2020-12-02 21:27:08 +0600

    Update cert.js

--- js/cert.js
+++ js/cert.js
@@ -6,12 +6,11 @@ firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
     uid = user.uid;
-  }else {
-      window.location.replace("../cert-auth.html");
   }
 });
     function logOut(){
         firebase.auth().signOut();
+        window.location.replace("../index.html");
     }
     mainApp.logOut = logOut;
 })()
