085fdd026a1cfc50c66ed85b088a6fb6ad15bf90
Maheyan <68837961+Maheyan@users.noreply.github.com>
2020-12-02 16:02:03 +0600

    trial

---
+++ js/firebase2.js
@@ -0,0 +1,17 @@
+
+var app_firebase = {};
+(function () { 
+    var firebaseConfig = {
+        apiKey: "AIzaSyB93HESocPC1vGx4YaNW3AIsc4am8yZwOc",
+        authDomain: "certificate-generator-auth.firebaseapp.com",
+        databaseURL: "https://certificate-generator-auth.firebaseio.com",
+        projectId: "certificate-generator-auth",
+        storageBucket: "certificate-generator-auth.appspot.com",
+        messagingSenderId: "818689700970",
+        appId: "1:818689700970:web:cf8d02f14bb1a66e4fc613"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  app_firebase = firebase;
+})()
