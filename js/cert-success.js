8c5f855a16ba9ed13c01ce5d17567bf0bf3d0d5a
Maheyan <68837961+Maheyan@users.noreply.github.com>
2020-12-02 19:08:04 +0600

    updated

--- js/cert-success.js
+++ js/cert-success.js
@@ -1,3 +1,62 @@
+function toggleSignIn() {
+  if (firebase.auth().currentUser) {
+    firebase.auth().signOut();
+  } else {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    if (email.length < 4) {
+      alert('Please enter an email address.');
+      return;
+    }
+    if (password.length < 4) {
+      alert('Please enter a password.');
+      return;
+    }
+    // Sign in with email and pass.
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode === 'auth/wrong-password') {
+        alert('Wrong password.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+      document.getElementById('quickstart-sign-in').disabled = false;
+    });
+  }
+  document.getElementById('quickstart-sign-in').disabled = true;
+}
+function initApp() {
+  // Listening for auth state changes.
+  firebase.auth().onAuthStateChanged(function(user) {
+    document.getElementById('quickstart-verify-email').style.display = 'none';
+    if (user) {
+  window.location.replace("certificate/index.html");
+    } else {
+      // User is signed out.
+      document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
+      document.getElementById('quickstart-sign-in').textContent = 'Sign in';
+      document.getElementById('quickstart-account-details').textContent = 'null';
+    }
+    document.getElementById('quickstart-sign-in').disabled = false;
+  });
+  // [END authstatelistener]
+
+  document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
+}
+
+window.onload = function() {
+  initApp();
+};
+
+
+
+
+
+
+
 var mainApp = {};
 (function() {
     var firebase = app_firebase;
