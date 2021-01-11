91b7e39782460e97dc7359fec3b4f18b09d2ad0f
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-01-11 18:23:52 +0600

    Update firebase2.js

--- js/firebase2.js
+++ js/firebase2.js
@@ -1,19 +1,20 @@
-var config = {
-    apiKey: "AIzaSyB93HESocPC1vGx4YaNW3AIsc4am8yZwOc",
-  authDomain: "certificate-generator-auth.firebaseapp.com",
-  databaseURL: "https://certificate-generator-auth.firebaseio.com",
-  projectId: "certificate-generator-auth",
-  storageBucket: "certificate-generator-auth.appspot.com",
-  messagingSenderId: "818689700970",
-  appId: "1:818689700970:web:cf8d02f14bb1a66e4fc613"
+var app_firebase = {};
+(function () { 
+    var firebaseConfig = {
+        apiKey: "AIzaSyBs_QL_YWhueXi5ZhYLeS9sxtnM42RIovo",
+        authDomain: "automation-10-ms.firebaseapp.com",
+        databaseURL: "https://automation-10-ms.firebaseio.com",
+        projectId: "automation-10-ms",
+        storageBucket: "automation-10-ms.appspot.com",
+        messagingSenderId: "631976397067",
+        appId: "1:631976397067:web:d364ba280da1e29f29fa0e",
+        measurementId: "G-787N0P3H46"
   };
-  firebase.initializeApp(config);
-  
-  // make auth and firestore references
-  const auth = firebase.auth();
-  const db = firebase.firestore();
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
-  // update firestore settings
-  db.settings({ timestampsInSnapshots: true });
-  
-  document.addEventListener("contextmenu",function(e){e.preventDefault()});
+  app_firebase = firebase;
+})()
+document.addEventListener("contextmenu", function(e) {
+  e.preventDefault();
+});
