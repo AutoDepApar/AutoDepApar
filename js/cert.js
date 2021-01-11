cdefc69fbf6c1cfe2a5d3cc9be769875e49838ed
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-01-11 18:36:03 +0600

    fix

--- js/cert.js
+++ js/cert.js
@@ -1,55 +1,53 @@
-  // listen for auth status changes
+var config = {
+  apiKey: "AIzaSyB93HESocPC1vGx4YaNW3AIsc4am8yZwOc",
+authDomain: "certificate-generator-auth.firebaseapp.com",
+databaseURL: "https://certificate-generator-auth.firebaseio.com",
+projectId: "certificate-generator-auth",
+storageBucket: "certificate-generator-auth.appspot.com",
+messagingSenderId: "818689700970",
+appId: "1:818689700970:web:cf8d02f14bb1a66e4fc613"
+};
+firebase.initializeApp(config);
+const auth = firebase.auth();
+const db = firebase.firestore();
+db.settings({ timestampsInSnapshots: true });
 auth.onAuthStateChanged(user => {
-  if (user) {
-    console.log('user logged in: ', user);
-    db.collection('guides').get().then(snapshot => {
-      setupGuides(snapshot.docs);
-    });    
-  } else {
-    console.log('user logged out');
-    setupGuides([]);
-  }
+if (user) {
+db.collection('guides').get().then(snapshot => {
+  setupGuides(snapshot.docs);
+});    
+} else {
+setupGuides([]);
+window.location.replace("../index.html");
+}
 })
 
-// logout
-const logout = document.querySelector('#logout');
+const logout = document.getElementById('logout');
 logout.addEventListener('click', (e) => {
-  e.preventDefault();
-  auth.signOut();
+e.preventDefault();
+auth.signOut();
+window.location.replace("../index.html");
 });
-
-
-
-
-// DOM elements
 const guideList = document.getElementById('guides');
-
-// setup guides
 const setupGuides = (data) => {
 
-  let html = '';
-  data.forEach(doc => {
-    const guide = doc.data();
-    const div = `
-    <div class="card-deck">
-    <div class="card">
-      <img class="card-img-top" src="${guide.image}" alt="Card image">
-    
-      <div class="card-img-overlay">
-          <h4 class="card-title-danger">${guide.title}</h4>
-          <a href="${guide.content}" target="_blank" class="btn btn-outline-danger">Click Here</a>
-      
-    </div>
-  </div>
-</div>
-    `;
-    html += div;
-  });
-  guideList.innerHTML = html
+let html = '';
+data.forEach(doc => {
+const guide = doc.data();
+const div = `
+<div class="card-deck">
+<div class="card">
+  <img class="card-img-top" src="${guide.image}" alt="Card image">
 
+  <div class="card-img-overlay">
+      <h4 class="card-title-danger">${guide.title}</h4>
+      <a href="${guide.content}" target="_blank" class="btn btn-outline-danger">Click Here</a>
+  
+</div>
+</div>
+</div>
+`;
+html += div;
+});
+guideList.innerHTML = html
 };
-// const logout = document.querySelector('#logout');
-// logout.addEventListener('click', (e) => {
-//   auth.signOut();
-//   window.location.replace("../index.html");
-// });
