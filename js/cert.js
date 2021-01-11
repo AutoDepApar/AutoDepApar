303b6a0081d9d18252451ffb4c056721f1ab84df
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-01-11 17:37:53 +0600

    fix

--- js/cert.js
+++ js/cert.js
@@ -1,34 +1,32 @@
+
+// listen for auth status changes
 auth.onAuthStateChanged(user => {
   if (user) {
-    db.collection('certData').get().then(snapshot => {
-      setData(snapshot.docs);
+    db.collection('guides').get().then(snapshot => {
+      setupGuides(snapshot.docs);
     });    
   } else {
+    setupGuides([]);
     window.location.replace("../index.html");
   }
 })
-const logout = document.querySelector('#logout');
-logout.addEventListener('click', (e) => {
-  auth.signOut();
-  window.location.replace("../index.html");
-});
 // DOM elements
-const certList = document.getElementById('certData');
+const guideList = document.getElementById('guides');
 
 // setup guides
-const setData = (data) => {
+const setupGuides = (data) => {
 
   let html = '';
   data.forEach(doc => {
-    const crtdata = doc.data();
+    const guide = doc.data();
     const div = `
     <div class="card-deck">
     <div class="card">
-      <img class="card-img-top" src="${crtdata.image}" alt="Card image">
+      <img class="card-img-top" src="${guide.image}" alt="Card image">
     
       <div class="card-img-overlay">
-          <h4 class="card-title-danger">${crtdata.title}</h4>
-          <a href="${crtdata.content}" target="_blank" class="btn btn-outline-danger">Click Here</a>
+          <h4 class="card-title-danger">${guide.title}</h4>
+          <a href="${guide.content}" target="_blank" class="btn btn-outline-danger">Click Here</a>
       
     </div>
   </div>
@@ -36,6 +34,12 @@ const setData = (data) => {
     `;
     html += div;
   });
-  certList.innerHTML = html
+  guideList.innerHTML = html
 
 };
+
+// const logout = document.querySelector('#logout');
+// logout.addEventListener('click', (e) => {
+//   auth.signOut();
+//   window.location.replace("../index.html");
+// });
