769485facdd6834b4ae754596bbe497fd4935e70
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-02-27 02:16:19 +0600

    Update cert.js

--- js/cert.js
+++ js/cert.js
@@ -1,43 +1,43 @@
 var config = {
-  apiKey: "AIzaSyB93HESocPC1vGx4YaNW3AIsc4am8yZwOc",
-authDomain: "certificate-generator-auth.firebaseapp.com",
-databaseURL: "https://certificate-generator-auth.firebaseio.com",
-projectId: "certificate-generator-auth",
-storageBucket: "certificate-generator-auth.appspot.com",
-messagingSenderId: "818689700970",
-appId: "1:818689700970:web:cf8d02f14bb1a66e4fc613"
+    apiKey: "AIzaSyB93HESocPC1vGx4YaNW3AIsc4am8yZwOc",
+    authDomain: "certificate-generator-auth.firebaseapp.com",
+    databaseURL: "https://certificate-generator-auth.firebaseio.com",
+    projectId: "certificate-generator-auth",
+    storageBucket: "certificate-generator-auth.appspot.com",
+    messagingSenderId: "818689700970",
+    appId: "1:818689700970:web:cf8d02f14bb1a66e4fc613"
 };
 firebase.initializeApp(config);
 const auth = firebase.auth();
 const db = firebase.firestore();
 db.settings({ timestampsInSnapshots: true });
 auth.onAuthStateChanged(user => {
-if (user) {
-db.collection('guides').get().then(snapshot => {
-  setupGuides(snapshot.docs);
-});    
-} else {
-setupGuides([]);
-window.location.replace("../index.html");
-}
+    if (user) {
+        db.collection('guides').get().then(snapshot => {
+            setupGuides(snapshot.docs);
+        });
+    } else {
+        setupGuides([]);
+        window.location.replace("../index.html");
+    }
 })
 
 const logout = document.getElementById('logout');
 logout.addEventListener('click', (e) => {
-e.preventDefault();
-auth.signOut();
-window.location.replace("../index.html");
+    e.preventDefault();
+    auth.signOut();
+    window.location.replace("../index.html");
 });
 const guideList = document.getElementById('guides');
 const setupGuides = (data) => {
 
-let html = '';
-data.forEach(doc => {
-const guide = doc.data();
-const div = `
+    let html = '';
+    data.forEach(doc => {
+        const guide = doc.data();
+        const div = `
 <div class="card-deck">
 <div class="card">
-  <img class="card-img-top" src="${guide.image}" alt="Card image">
+  <img class="card-img-bottom" src="${guide.image}" alt="Card image">
 
   <div class="card-img-overlay">
       <h4 class="card-title-danger">${guide.title}</h4>
@@ -47,10 +47,10 @@ const div = `
 </div>
 </div>
 `;
-html += div;
-});
-guideList.innerHTML = html
+        html += div;
+    });
+    guideList.innerHTML = html
 };
 document.addEventListener("contextmenu", function(e) {
-  e.preventDefault();
+    e.preventDefault();
 });
